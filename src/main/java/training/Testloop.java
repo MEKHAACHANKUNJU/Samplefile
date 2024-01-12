2b78978a1ded829bc4137d0a4db6ab2207656b50
MEKHAACHANKUNJU <144969731+MEKHAACHANKUNJU@users.noreply.github.com>
2024-01-12 20:52:26 +0530

    Commit

---
+++ src/main/java/training/Testloop.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package training;
+
+/**
+ * 
+ */
+public class Testloop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
